098e787c5b46c810d7eb857c506976eb0af3d83e
BlackmythCZ <68425625+BlackmythCZ@users.noreply.github.com>
2021-11-22 15:46:46 +0100

    Update test_tah_na_prazdne_pole.py

--- Testy/test_tah_na_prazdne_pole.py
+++ Testy/test_tah_na_prazdne_pole.py
@@ -1,4 +1,7 @@
+# Testům, které kontrolují, že se program za správných podmínek chová správně, se říká pozitivní testy. Test může vypadat třeba takhle:
+
 import piskvorky1d
+
 def test_tah_na_prazdne_pole()
     pole = piskvorky.tah_pocitace('--------------------')
     assert len(pole) == 20

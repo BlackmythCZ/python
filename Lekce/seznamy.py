a370680776ebd5566b5ead4e5950eebe073e4ecf
Tomáš Hampl <htmpl@email.cz>
2021-11-24 14:45:47 +0100

    more about lists

--- Lekce/seznamy.py
+++ Lekce/seznamy.py
@@ -49,6 +49,34 @@ cisla = [1, 2, 3, 4]
 cisla[1:-1] = [6, 5]
 print(cisla)
 
+# Přiřazením do podseznamu se dá i změnit délka seznamu, nebo některé prvky úplně odstranit:
+cisla = [1, 2, 3, 4]
+cisla[1:-1] = [0, 0, 0, 0, 0]
+print(cisla)
+cisla[1:-1] = []
+print(cisla)
 
+# Tenhle výše uvedený zápis pro mazání prvků je ale docela nepřehledný, a proto na to máme zvláštní příkaz jménem del:
+cisla = [1, 2, 3, 4, 5, 6]
+del cisla[-1]
+print(cisla)
+del cisla[3:5]
+print(cisla)
+del cisla # Maže i proměnné!
+#print(cisla)
 
+# Další mazací metody jsou:
+# pop, která odstraní a vrátí poslední prvek v seznamu – například pokud mám seznam karet v balíčku, jde takhle jednoduše „líznout” kartu,
+# remove, která najde v seznamu daný prvek a odstraní ho,
+# clear, která vyprázdní celý seznam.
 
+cisla = [1, 2, 3, 'abc', 4, 5, 6, 12]
+posledni = cisla.pop()
+print(posledni)
+print(cisla)
+
+cisla.remove('abc')
+print(cisla)
+
+cisla.clear()
+print(cisla)
